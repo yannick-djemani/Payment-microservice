591499df68eed252916b3f32fdb7485fca91091e
yannick Djemani <yanger842@gmail.com>
2023-03-22 22:09:21 -0400

    disable security in payment  service

--- src/main/java/com/codeimmig/PaymentService/security/WebSecurityConfig.java
+++ src/main/java/com/codeimmig/PaymentService/security/WebSecurityConfig.java
@@ -1,19 +1,27 @@
-package com.codeimmig.PaymentService.security;
-
-/*@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/payment/**")
-                                .hasAuthority("SCOPE_internal")
-                                .anyRequest()
-                                .authenticated())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-        return http.build();
-    }
-
-}*/
+//package com.codeimmig.PaymentService.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                .antMatchers("/payment/**")
+//                                .hasAuthority("SCOPE_internal")
+//                                .anyRequest()
+//                                .authenticated())
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+//        return http.build();
+//    }
+//
+//}
